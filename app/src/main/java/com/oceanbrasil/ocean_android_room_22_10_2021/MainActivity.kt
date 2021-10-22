388ea25f7ec18f77b1a5db1671203f58260b187b
Paulo Salvatore <salvatoregames@gmail.com>
2021-10-22 11:30:47 -0300

    feat(view): add `PokemonViewModel` instance to `MainActivity`

--- app/src/main/java/com/oceanbrasil/ocean_android_room_22_10_2021/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_room_22_10_2021/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.oceanbrasil.ocean_android_room_22_10_2021
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.oceanbrasil.ocean_android_room_22_10_2021.viewmodel.PokemonViewModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val pokemonViewModel: PokemonViewModel =
+            ViewModelProvider(this)
+                .get(PokemonViewModel::class.java)
+
+        val tvPokemons = findViewById<TextView>(R.id.tvPokemons)
+
+        tvPokemons.text = pokemonViewModel.pokemons.count().toString()
     }
 }
