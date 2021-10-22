d7bba7b01b4628174b0a56e40a18bad232a299dd
Paulo Salvatore <salvatoregames@gmail.com>
2021-10-22 11:30:34 -0300

    feat(view-model): implement `PokemonViewModel`

---
+++ app/src/main/java/com/oceanbrasil/ocean_android_room_22_10_2021/viewmodel/PokemonViewModel.kt
@@ -0,0 +1,18 @@
+package com.oceanbrasil.ocean_android_room_22_10_2021.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.oceanbrasil.ocean_android_room_22_10_2021.model.AppDatabase
+import com.oceanbrasil.ocean_android_room_22_10_2021.model.PokemonEntity
+
+class PokemonViewModel(application: Application) : AndroidViewModel(application) {
+    val pokemons: List<PokemonEntity>
+
+    init {
+        val db = AppDatabase.getDatabase(application)
+
+        val pokemonDao = db.pokemonDao()
+
+        pokemons = pokemonDao.findAll()
+    }
+}
